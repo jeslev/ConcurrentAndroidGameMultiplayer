0b6f056b42246f89f6811da14ff97d4ea4f37c53
maritzalapa <maritzalapa@gmail.com>
2015-10-06 22:31:08 -0500

    Anhadiendo Fondo y Nave 2

--- app/src/main/java/com/example/jeslev/concurrentgamenetwork/ScenarioActivity.java
+++ app/src/main/java/com/example/jeslev/concurrentgamenetwork/ScenarioActivity.java
@@ -2,6 +2,7 @@ package com.example.jeslev.concurrentgamenetwork;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -17,7 +18,9 @@ public class ScenarioActivity extends AppCompatActivity {
     private final Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final Paint paintServer = new Paint(Paint.ANTI_ALIAS_FLAG);
-
+    private Bitmap scaled;
+    int xx = 100;
+    int yy = 150;
 
     public SurfaceView SurfaceViewX;
 
@@ -53,10 +56,10 @@ public class ScenarioActivity extends AppCompatActivity {
             super(context);
             // ホルダーのコールバックに自身をセットすることで、準備だできた時点で呼び出される
             getHolder().addCallback(this);
-            /*bitmapOff = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.dibujito01);
+            bitmapOff = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.spaceship);
             bitmapOn = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.dibujito02);////////////////// */
+                    R.drawable.spaceship2);//////////////////
         }
 
          /*public void ServidorEnviarCoordenadas(){
@@ -76,37 +79,37 @@ public class ScenarioActivity extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(), "click ARRIBA", Toast.LENGTH_SHORT).show();
                 System.out.println(" Click ARRIBA");
 
-                //yy = yy - 2;
+                yy = yy - 5;
             }
 
             if( 70 < event.getX() && event.getX()<130 && (wy-180)< event.getY() && event.getY()<(wy-120) ){
                 //Toast.makeText(getApplicationContext(), "click ABAJO", Toast.LENGTH_SHORT).show();
                 System.out.println(" Click ABAJO");
-                //yy = yy  + 2;
+                yy = yy  + 5;
             }
 
             if( 20 < event.getX() && event.getX()<80 && (wy-230)< event.getY() && event.getY()<(wy-170) ){
                 //Toast.makeText(getApplicationContext(), "click IZQUI", Toast.LENGTH_SHORT).show();
                 System.out.println(" Click IZQUIERDA");
-                //xx = xx  - 2;
+                xx = xx  - 5;
             }
 
             if( 120 < event.getX() && event.getX()<180 && (wy-230)< event.getY() && event.getY()<(wy-170) ){
                 //Toast.makeText(getApplicationContext(), "click DER", Toast.LENGTH_SHORT).show();
                 System.out.println(" Click DERECHA");
-                //??xx = xx  + 2;
+                xx = xx  + 5;
             }
 
             if( (wx-280) < event.getX() && event.getX()<(wx-220) && (wy-280)< event.getY() && event.getY()<(wy-220) ){
                 //Toast.makeText(getApplicationContext(), "click DER", Toast.LENGTH_SHORT).show();
                 System.out.println(" Click A");
-                //??xx = xx  + 2;
+                xx = xx  + 5;
             }
 
             if( (wx-280) < event.getX() && event.getX()<(wx-220) && (wy-180)< event.getY() && event.getY()<(wy-120) ){
                 //Toast.makeText(getApplicationContext(), "click DER", Toast.LENGTH_SHORT).show();
                 System.out.println(" Click B");
-                //??xx = xx  + 2;
+                xx = xx  + 5;
             }
 
             //ServidorEnviarCoordenadas();
@@ -171,14 +174,21 @@ public class ScenarioActivity extends AppCompatActivity {
             //canvas.drawColor(Color.GRAY);
             int wx = getWidth();
             int wy = getHeight();
-            canvas.drawColor(Color.BLACK);
+            Bitmap background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+            float scale = (float) background.getHeight() / (float) getHeight();
+            int newWidth = Math.round(background.getWidth() / scale);
+            int newHeight = Math.round(background.getHeight() / scale);
+            //scaled = Bitmap.createScaledBitmap(background, newWidth, newHeight, true);
+            scaled = Bitmap.createScaledBitmap(background, wx, wy, true);
+
+            canvas.drawBitmap(scaled, 0, 0, null);
             //XYPosition pos = null;
 
             //if (flat == 1){
             //    pos = imageXYposition(canvas, bitmap, new XYPosition(centerX,centerY));
             //}
-
-            canvas.drawCircle(100, wy-250, 30, paint2); // up arrow
+            System.out.println("wx: " + wx + ",wy: "+wy);
+                    canvas.drawCircle(100, wy - 250, 30, paint2); // up arrow
 
             canvas.drawCircle(100, wy-150, 30, paint2); // down arrow
 
@@ -196,32 +206,32 @@ public class ScenarioActivity extends AppCompatActivity {
             //if (flat == 1){
             //    canvas.drawBitmap(bitmap, pos.getX(), pos.getY(), null);
             //}
+            canvas.drawBitmap(bitmap, xx, yy, null);
             holder.unlockCanvasAndPost(canvas);
 
 
         }
 
-		/*private void doDraw(SurfaceHolder holder,Bitmap bitmap,int centerX, int centerY) {
+		private void doDraw(SurfaceHolder holder,Bitmap bitmap,int centerX, int centerY) {
 			Canvas canvas = holder.lockCanvas();
 
 			//canvas.drawColor(Color.GRAY);
 
 
-
-			canvas.drawColor(Color.BLACK);
+            canvas.drawColor(Color.BLACK);
 			XYPosition pos = imageXYposition(canvas, bitmap, new XYPosition(centerX,centerY));
 
 			canvas.drawCircle(50, 50, 25, paint2);
 
 			canvas.drawCircle(50, 150, 25, paint2);
 
-			canvas.drawCircle(25, 100, 25, paint2);
+            canvas.drawCircle(25, 100, 25, paint2);
 
-			canvas.drawCircle(75, 100, 25, paint2);
+            canvas.drawCircle(75, 100, 25, paint2);
 
-			canvas.drawRect(xx, yy, xx+10, yy+10, paint3);
+			//canvas.drawRect(xx, yy, xx+10, yy+10, paint3);
 
-			canvas.drawText("Envio FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF  x=" + xx + " y=" + yy, 50, 10,paintServerEnvia);
+	    //		canvas.drawText("Envio FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF  x=" + xx + " y=" + yy, 50, 10,paintServerEnvia);
 
 
 			canvas.drawBitmap(bitmap, pos.getX(), pos.getY(), null);
@@ -231,38 +241,67 @@ public class ScenarioActivity extends AppCompatActivity {
 			holder.unlockCanvasAndPost(canvas);
 
 
-		}*/
+		}
 
-//        private XYPosition imageXYposition(Canvas canvas, Bitmap bitmap,XYPosition position) {
-//            int bitmapHeight = bitmap.getHeight();
-//            int bitmapWidth = bitmap.getWidth();
-//            int canvasWidth = canvas.getWidth();
-//            int canvasHeight = canvas.getHeight();
-////Log.d("TouchEvent", "bitmapHeight : " + bitmapHeight + ", bitmapWidth : "+bitmapWidth +", canvasWidth : "+canvasWidth+", canvasHeight : "+canvasHeight);
-//            int x = position.getX() - bitmapWidth / 2;
-//            if (x < 0) {
-//                x = 0;
-//            }
-//            if (x > canvasWidth - bitmapWidth) {
-//                x = canvasWidth - bitmapWidth;
-//            }
-//            int y = position.getY() - bitmapHeight / 2;
-//            if (y < 0) {
-//                y = 0;
-//            }
-//            if (y > canvasHeight - bitmapHeight) {
-//                y = canvasHeight - bitmapHeight;
-//            }
-//            //Log.d("TouchEvent", "x : " + x + ", y : "+ y);
-//            //System.out.println("TouchEvent"+ "x : " + x + ", y : "+ y);
-//            return new XYPosition(x, y);
-//
-//
-//
-//
-//
-//        }
+        private XYPosition imageXYposition(Canvas canvas, Bitmap bitmap,XYPosition position) {
+            int bitmapHeight = bitmap.getHeight();
+            int bitmapWidth = bitmap.getWidth();
+            int canvasWidth = canvas.getWidth();
+            int canvasHeight = canvas.getHeight();
+//Log.d("TouchEvent", "bitmapHeight : " + bitmapHeight + ", bitmapWidth : "+bitmapWidth +", canvasWidth : "+canvasWidth+", canvasHeight : "+canvasHeight);
+            int x = position.getX() - bitmapWidth / 2;
+            if (x < 0) {
+                x = 0;
+            }
+            if (x > canvasWidth - bitmapWidth) {
+                x = canvasWidth - bitmapWidth;
+            }
+            int y = position.getY() - bitmapHeight / 2;
+            if (y < 0) {
+                y = 0;
+            }
+            if (y > canvasHeight - bitmapHeight) {
+                y = canvasHeight - bitmapHeight;
+            }
+            //Log.d("TouchEvent", "x : " + x + ", y : "+ y);
+            //System.out.println("TouchEvent"+ "x : " + x + ", y : "+ y);
+            return new XYPosition(x, y);
+
+
+
+
+
+        }
 
     }
+    public class XYPosition {
 
+        private int x;
+        private int y;
+        XYPosition(){
+            this(0,0);
+        }
+        XYPosition(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+
+        }
+
+        public void setX(int x) {
+            this.x = x;
+
+        }
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+    }
 }
